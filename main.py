7891aff4a55daa11c42d8eedaafc8a7516eb39da
Alexander Samrega <alexander.samrega@gmail.com>
2019-10-01 21:37:01 +0300

    Add sequential algorithm

---
+++ main.py
@@ -0,0 +1,69 @@
+import pathlib
+import time
+
+import networkx as nx
+
+
+def search_maximum_clique(graph):
+    result = set()
+    expand(graph, set(), degree_sort(graph, graph.nodes), result)
+    return result
+
+
+def expand(graph, candidate, neighbors, best):
+    colors, colored_nodes = colourise(graph, neighbors)
+    for i, colored_neighbor in zip(reversed(range(len(colored_nodes))), reversed(colored_nodes)):
+        if len(candidate) + colors[i] > len(best):
+            node = colored_neighbor
+            candidate.add(node)
+            new_neighbors = [n for n in neighbors if n in graph.neighbors(node)]
+            if not new_neighbors:
+                if len(candidate) > len(best):
+                    best.clear()
+                    best.update(candidate)
+            else:
+                expand(graph, candidate, new_neighbors, best)
+            candidate.remove(node)
+            colored_nodes.pop()
+            neighbors.remove(node)
+
+
+def colourise(graph, neighbors):
+    coloured_buckets = []
+    for node in neighbors:
+        for bucket in coloured_buckets:
+            for colored_node in bucket:
+                if graph.has_edge(node, colored_node):
+                    break
+            else:
+                bucket.append(node)
+                break
+        else:
+            coloured_buckets.append([node])
+
+    colors = []
+    colored_nodes = []
+    for i, bucket in enumerate(coloured_buckets, start=1):
+        for node in bucket:
+            colored_nodes.append(node)
+            colors.append(i)
+
+    return colors, colored_nodes
+
+
+def degree_sort(graph, nodes):
+    return sorted(nodes, key=graph.degree, reverse=True)
+
+
+def main():
+    graph_path = pathlib.Path(pathlib.Path.cwd(), 'graphs')
+    graph = nx.algorithms.operators.unary.complement(nx.read_edgelist(graph_path.joinpath('graph2.txt')))
+
+    x = time.time()
+    clique = search_maximum_clique(graph)
+    print(time.time() - x)
+    print(clique)
+
+
+if __name__ == '__main__':
+    main()
